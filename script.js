e0ad19210e2c627609b3a61f25a591ecf18b6a9a
loetgott <KingOfCrochet@web.de>
2024-08-25 00:24:41 +0200

    table login 1

--- script.js
+++ script.js
@@ -60,9 +60,7 @@ let currentUserUID = null;
 let loggedIn = false;
 
 function setLoggedIn(nLoggedIn){
-  console.log("eingeloggt!");
   loggedIn = nLoggedIn;
-  console.log(loggedIn);
 }
 
 function getLoggedIn(){
