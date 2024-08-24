89634c09efea8cf618ea35666d95131da3b8bf75
loetgott <KingOfCrochet@web.de>
2024-08-24 14:05:31 +0200

    login test 4

--- login.js
+++ login.js
@@ -1,3 +1,13 @@
+// Deine Firebase Konfiguration und Initialisierung hier
+const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_PROJECT_ID.appspot.com",
+  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
+  appId: "YOUR_APP_ID"
+};
+
 const app = firebase.initializeApp(firebaseConfig);
 const auth = firebase.auth();
 
