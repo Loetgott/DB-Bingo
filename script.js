a9a72e1b0e44fb60a4994c698d261094bf4f9a62
loetgott <KingOfCrochet@web.de>
2024-08-25 11:41:01 +0200

    data save bugfix

--- script.js
+++ script.js
@@ -41,7 +41,7 @@ async function loadTable() {
 
 function saveData(playerId, fieldName, fieldValue) {
 
-  const docRef = db.collection("scores").doc(String(playerId));
+  const docRef = db.collection("player").doc(String(playerId));
 
   docRef.set({
     [fieldName] : fieldValue,
